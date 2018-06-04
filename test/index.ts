d63bb9e1dcdbd6018803c909bdc25c17a6eb4691
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-04 18:01:51 +0700

    Add Test

---
+++ test/index.ts
@@ -0,0 +1,15 @@
+// import { expect } from 'chai';
+import 'mocha';
+// import index from '../src/index';
+
+describe('Hello function', () => {
+    // it('should be return hello world', () => {
+    //     // const result = index();
+    //     // expect(result).to.equal('Hello world');
+    // });
+
+    // it('should be return Number', () => {
+    //     // const result = index();
+    //     // expect(typeof result).to.equal('string');
+    // });
+});
